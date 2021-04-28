d7bbca9b14625193006d33bbda994a1f5c8211a3
hellman <hellman@noreply.github.com>
2021-04-28 21:40:58 +0200

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='optisolveapi',
+    version='0.1.0',
+    packages=["optisolveapi"],
+
+    url=None,
+    license="MIT",
+
+    author='Aleksei Udovenko',
+    author_email="aleksei@affine.group",
+    maintainer=None,
+    maintainer_email=None,
+
+    description="""Optimization & Solving common API (SAT, MILP, etc.)""",
+    long_description=None,
+
+    python_requires='>=3.5,<4.0',
+    install_requires=[
+        'python-sat[pblib,aiger]',
+    ],
+)
