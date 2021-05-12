7e1803f9d9dd13e75ba11d25af6815aa498c8e97
hellman <hellman@users.noreply.github.com>
2021-05-12 15:27:40 +0200

    allow re-registering, but warn

--- optisolveapi/base.py
+++ optisolveapi/base.py
@@ -1,10 +1,18 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
 class SolverBase:
     BY_SOLVER = NotImplemented  # to be defined in the collection class
 
     @classmethod
     def register(cls, name):
         def deco(subcls):
-            assert name not in cls.BY_SOLVER
+            if name in cls.BY_SOLVER:
+                log.warning(
+                    f"re-registering solver {name} in class {cls.__name__}"
+                )
             cls.BY_SOLVER[name.lower()] = subcls
             return subcls
         return deco
